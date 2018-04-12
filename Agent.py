4f12dea4feeb03a73c267444db628b73f0c81a50
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-04-12 23:39:39 +0900

    変数の定義，Panelの実装

--- Agent.py
+++ Agent.py
@@ -1,12 +1,12 @@
 class Agent:
-	_point: (int, int) #座標
+	_point: [int, int] #座標
 	_team: int #所属チーム
 
-	def __init__(self, point, team): #エージェント生成(point:座標,team:チーム)
+	def __init__(self, point:[int, int], team:int): #エージェント生成(point:座標,team:チーム)
 		pass
 
-	def new(point,team): #コンストラクタ呼び出し(point:座標,team:チーム)
+	def new(point:[int, int], team:int): #コンストラクタ呼び出し(point:座標,team:チーム)
 		return Agent(point,team)
 
-	def move(self,vector): #エージェントを動かす(vector:方向)
+	def move(self,vector:[int, int]): #エージェントを動かす(vector:方向)
 		pass
--- Panel.py
+++ Panel.py
@@ -1,15 +1,16 @@
 class Panel:
 	_score: int #パネルが持つ点数
-	_state: int #パネルの状態
+	_state: int #パネルの状態(0が中立，1が1P，2が2P)
 	
-	def __init__(self, score): #パネル生成
-		pass
+	def __init__(self, score:int): #パネル生成
+		self._score = score
+		self._state = 0
 	
-	def new(score): #コンストラクタ呼び出し
+	def new(score:int): #コンストラクタ呼び出し
 		return Panel(score)
 
-	def mkcard(self, team): #パネルにカードを置く(team:チーム)
-		pass
+	def mkcard(self, team:int): #パネルにカードを置く(team:チーム)
+		self._state = team
 
 	def rmcard(self): #パネルに置いているカードを除去する
-		pass
+		self._state = 0
