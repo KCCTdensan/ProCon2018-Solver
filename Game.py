9a04472e12194a05db4ef75524a0790e3524efd8
kamejiro <r116309@g.kobe-kosen.ac.jp>
2018-04-13 23:00:24 +0900

    スコア計算

--- Game.py
+++ Game.py
@@ -17,15 +17,64 @@ class Game:
 		return Game()
 
 	def score(self): #得点計算
-		_1Pscore = 0
-		_2Pscore = 0
-		for x in range(len(_Panels)):
-			for y in range(len(_Panels[x])):
-				p = _Panels[x][y]
-				if p.getState() == 1:
-					_1Pscore += p.getScore()
-				elif p.getState() == 2:
-					_2Pscore += p.getScore()
+		panelPoint1 = 0
+		panelPoint2 = 0
+		regionPoint1 = 0
+		regionPoint2 = 0
+		NumY = len(_Panels)
+		NumX = len(_Panels[0])
+		searchedPanels = np.zeros_like(_Panels, dtype = np.bool)
+
+		def regionPoint(self, x:int, y:int, player:int)->int:
+			if searchedPanels[y][x]:
+				return 0
+			searchedPanels[y][x] = True
+			if (y == 0)or(y == NumY - 1)or(x == 0)or(x == NumX - 1):
+				return -1
+			if _Panels[y][x].getState() == player:
+				return -2
+			l = regionPoint(self, x - 1, y, player)
+			if l == -1:
+				return -1
+			elif l == -2:
+				return abs(_Panels[y][x].getScore())
+			t = regionPoint(self, x, y - 1, player)
+			if t == -1:
+				return -1
+			elif l == -2:
+				return abs(_Panels[y][x].getScore())
+			r = regionPoint(self, x + 1, y, player)
+			if r == -1:
+				return -1
+			elif l == -2:
+				return abs(_Panels[y][x].getScore())
+			b = regionPoint(self, x, y + 1, player)
+			if b == -1:
+				return -1
+			elif l == -2:
+				return abs(_Panels[y][x].getScore())
+			return l + t + r + b
+
+		for y in range(NumY):
+			for x in range(NumX):
+				p = _Panels[y][x]
+				state = p.getState()
+				if state == 1:
+					panelPoint1 += p.getScore()
+				elif state == 2:
+					panelPoint2 += p.getScore()
+		for y in range(NumY):
+			for x in range(NumX):
+				if not searchedPanels[y][x]:
+					regionPoint1 = regionPoint(self, x, y, 1)
+					if regionPoint >= 0:
+						break
+		for y in range(NumY):
+			for x in range(NumX):
+				if not searchedPanels[y][x]:
+					regionPoint2 = regionPoint(self, x, y, 2)
+					if regionPoint >= 0:
+						break
 
 	def main(self):
 		#for turn in _turn:
