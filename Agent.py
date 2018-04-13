8a59119863e2b5d046503f418113b9561d9e9b4c
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-04-14 00:39:12 +0900

    Agent,Panelのnew()削除,クラス変数→インスタンス変数

--- Agent.py
+++ Agent.py
@@ -1,15 +1,9 @@
 import numpy as np
 
 class Agent:
-	_point: np.array([int, int]) #座標
-	_team: int #所属チーム
-
 	def __init__(self, point:[int, int], team:int): #エージェント生成(point:座標,team:チーム)
-		self._point = np.array(point)
-		self._team = team
-
-	def new(point:[int, int], team:int): #コンストラクタ呼び出し(point:座標,team:チーム)
-		return Agent(point,team)
+		self._point = np.array(point) #座標
+		self._team = team #所属チーム(1or2)
 
 	def move(self,vector:[int, int]): #エージェントを動かす(vector:方向)
 		self._point += np.array(vector)
--- Panel.py
+++ Panel.py
@@ -1,14 +1,8 @@
 class Panel:
-	_score: int #パネルが持つ点数
-	_state: int #パネルの状態(0が中立，1が1P，2が2P)
-	
 	def __init__(self, score:int): #パネル生成
-		self._score = score
-		self._state = 0
+		self._score = score #パネルが持つ点数
+		self._state = 0 #パネルの状態(0が中立，1が1P，2が2P)
 	
-	def new(score:int): #コンストラクタ呼び出し
-		return Panel(score)
-
 	def mkcard(self, team:int): #パネルにカードを置く(team:チーム)
 		self._state = team
 
