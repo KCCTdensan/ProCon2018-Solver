1da903beab2fd4ab6b63fd765ac21b22e66771f2
kamejiro <r116309@g.kobe-kosen.ac.jp>
2018-04-14 00:40:48 +0900

    Score()

--- Game.py
+++ Game.py
@@ -5,13 +5,24 @@ from Window import *
 
 class Game:
 	_turn: int #最終ターン数
-	_1Pscore: int #1Pの得点
-	_2Pscore: int #2Pの得点
+	_1PpanelScore: int #1Pの得点
+	_1PregionScore: int #1Pの得点
+	_2PpanelScore: int #2Pの得点
+	_2PregionScore: int #2Pの得点
 	_Panels: np.array #ステージを構成するパネルのリスト
 	_Agents: list #ステージに存在するエージェントのリスト
 
 	def __init__(self): #ステージ生成
-		pass
+		self.Panels = np.array([\
+			[0, 0, 0, 0, 0, 0, 0],\
+			[0, 0, 0, 1, 0, 0, 0],\
+			[0, 0, 1, 0, 0, 0, 0],\
+			[0, 0, 0, 0, 0, 0, 0],\
+			[0, 0, 0, 0, 0, 0, 0],\
+			[0, 0, 0, 0, 0, 0, 0],\
+			[0, 0, 0, 0, 0, 0, 0],\
+			[0, 0, 0, 0, 0, 0, 0],\
+			]) #???
 
 	def new(): #コンストラクタ呼び出し
 		return Game()
@@ -21,60 +32,70 @@ class Game:
 		panelPoint2 = 0
 		regionPoint1 = 0
 		regionPoint2 = 0
-		NumY = len(_Panels)
-		NumX = len(_Panels[0])
-		searchedPanels = np.zeros_like(_Panels, dtype = np.bool)
+		self.NumY = len(self.Panels)
+		self.NumX = len(self.Panels[0])
+		searchedPanels = [np.zeros_like(self.Panels, dtype = np.bool), np.zeros_like(self.Panels, dtype = np.bool)]
 
 		def regionPoint(self, x:int, y:int, player:int)->int:
-			if searchedPanels[y][x]:
+			if searchedPanels[player - 1][y][x]:
 				return 0
-			searchedPanels[y][x] = True
-			if (y == 0)or(y == NumY - 1)or(x == 0)or(x == NumX - 1):
+			searchedPanels[player - 1][y][x] = True
+			if (y == 0)or(y == self.NumY - 1)or(x == 0)or(x == self.NumX - 1):
 				return -1
-			if _Panels[y][x].getState() == player:
+			if self.Panels[y][x] == player:
 				return -2
 			l = regionPoint(self, x - 1, y, player)
 			if l == -1:
 				return -1
 			elif l == -2:
-				return abs(_Panels[y][x].getScore())
+				return abs(self.Panels[y][x].getScore())
 			t = regionPoint(self, x, y - 1, player)
 			if t == -1:
 				return -1
 			elif l == -2:
-				return abs(_Panels[y][x].getScore())
+				return abs(self.Panels[y][x].getScore())
 			r = regionPoint(self, x + 1, y, player)
 			if r == -1:
 				return -1
 			elif l == -2:
-				return abs(_Panels[y][x].getScore())
+				return abs(self.Panels[y][x].getScore())
 			b = regionPoint(self, x, y + 1, player)
 			if b == -1:
 				return -1
 			elif l == -2:
-				return abs(_Panels[y][x].getScore())
+				return abs(self.Panels[y][x].getScore())
 			return l + t + r + b
 
-		for y in range(NumY):
-			for x in range(NumX):
-				p = _Panels[y][x]
-				state = p.getState()
+		for y in range(self.NumY):
+			for x in range(self.NumX):
+				p = self.Panels[y][x]
+				state = p
 				if state == 1:
 					panelPoint1 += p.getScore()
 				elif state == 2:
 					panelPoint2 += p.getScore()
-		for y in range(NumY):
-			for x in range(NumX):
-				if not searchedPanels[y][x]:
+		for y in range(self.NumY):
+			for x in range(self.NumX):
+				if not searchedPanels[0][y][x]:
 					regionPoint1 = regionPoint(self, x, y, 1)
-					if regionPoint >= 0:
+					if regionPoint1 >= 0:
 						break
-		for y in range(NumY):
-			for x in range(NumX):
-				if not searchedPanels[y][x]:
+		for y in range(self.NumY):
+			for x in range(self.NumX):
+				if not searchedPanels[1][y][x]:
 					regionPoint2 = regionPoint(self, x, y, 2)
-					if regionPoint >= 0:
+					if regionPoint2 >= 0:
 						break
+		self.panelScore1P = panelPoint1
+		self.regionScore1P = regionPoint1
+		self.panelScore2P = panelPoint2
+		self.regionScore2P = regionPoint2
+
+	def printScore(self): #デバッグ用
+		return print(self.panelScore1P, self.regionScore1P, self.panelScore1P, self.regionScore2P)
+
+	def printPanel(self): #デバッグ用
+		print(self.Panels)
 
 	def main(self):
 		#for turn in _turn:
