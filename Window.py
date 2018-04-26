1dae134474e63fac051ae704d906f5919c133529
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-04-27 01:59:40 +0900

    盤面表示，意思入力

--- Window.py
+++ Window.py
@@ -9,21 +9,30 @@ class Window(ttk.Frame):
 		master.title("ProCon2018-Solver")
 		master.geometry("1260x720")
 		self._Game = Game
-		self.pack()
 		self.create_widgets()
+		self.pack()
 
 	def create_widgets(self):
 		#盤面
+		self.frame = tk.LabelFrame(self, bd = 2, relief = "ridge", text = "stage")
+		self.frame.pack(fill = "x")
+		
+		color = {0:"white", 1:"red", 2:"blue"}
+		for i in range(12):
+			for j in range(12):
+				#Panel = self._Game.getPanels[i][j]
+				#label = tk.Label(frame, text = Panel.getScore(), bg = color[Panel.getState()] relief = tk.RIDGE, bd = 2)
+				label = tk.Label(self.frame,	text ="1", bg = color[0], relief = tk.RIDGE, bd = 2)
+				label.grid(row = i, column = j)
 
 		#意思表示入力ボックス
+		self.entry = tk.Entry(self, font = ("",12), justify = "center", width = 20)
+		self.entry.pack()
+		self.button = tk.Button(self, text = "決定", font = ("", 12), width = 5, bg = "gray", command = self.push)
+		self.button.pack()
 
-		#quitボタン
-		self.quit = tk.Button(self, text="QUIT", fg = "red", command = self.destroy)
-		self.quit.pack(side="bottom")
-
-	def input(): #エージェントの意思をGameに渡す
-		self._Game.action()
-		self._Game.score()
-
-	def update(): #盤面を更新する
-		pass
+	def push(self): #ボタンが押された際，エージェントの意思をGameに渡す
+		print(self.entry.get())
+		#self._Game.action(self.entry.get())
+		#self._Game.score()
+		#GUIの更新
