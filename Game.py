d7f08ffcd7a2e4e508046dc1f6857f7b95c5bd3a
kamejiro <r116309@g.kobe-kosen.ac.jp>
2018-05-27 23:04:19 +0900

    変更途中

--- Game.py
+++ Game.py
@@ -13,8 +13,8 @@ class Game:
 		self._turn = Ran.randint(60, 120) #最終ターン数
 		self._1Pscore = 0 #1Pの得点
 		self._2Pscore = 0 #2Pの得点
-		self._1PIntention: [[int, int]] * 2
-		self._2PIntention: [[int, int]] * 2 #エージェント(4人)の意思([int, int])を保存する変数のリスト
+		#self._1PIntention: [[int, int]] * 2
+		#self._2PIntention: [[int, int]] * 2 #エージェント(4人)の意思([int, int])を保存する変数のリスト
 		#配列yの中に配列xが入る構造。定義時に行の中に列が入り込む、numpy.Zerosの仕様に基づく。
 		_xLen = Ran.randint(3, 12)
 		_yLen = Ran.randint(3, 12)
@@ -112,44 +112,47 @@ class Game:
 
 	def action(self,P1Intentions:list, P2Intentions:list): #エージェントの意思をみて，実際に移動orパネル操作
 		#引数 P1Intensions:[[x,y],[x,y]]、P2Intentions:[[x,y],[x,y]]
+
+		Intentions = [[P1Intentions[0][1], P1Intentions[0][0]], [P1Intentions[1][1], P1Intentions[1][0]], [P2Intentions[0][1], P2Intentions[0][0]], [P2Intentions[1][1], P2Intentions[1][0]]]
+
 		
 		#[[y,x],[y,x]]の形にするために中身をひっくり返して代入
-		self._1PIntention[0][0], self._1PIntention[0][1] = P1Intentions[0][1], P1Intentions[0][0]
-		self._1PIntention[1][0], self._1PIntention[1][1] = P1Intentions[1][1], P1Intentions[1][0]
-		self._2PIntention[0][0], self._2PIntention[0][1] = P2Intentions[0][1], P2Intentions[0][0]
-		self._2PIntention[1][0], self._2PIntention[1][1] = P2Intentions[1][1], P2Intentions[1][0]
+		_1PIntention[0][0], _1PIntention[0][1] = P1Intentions[0][1], P1Intentions[0][0]
+		_1PIntention[1][0], _1PIntention[1][1] = P1Intentions[1][1], P1Intentions[1][0]
+		_2PIntention[0][0], _2PIntention[0][1] = P2Intentions[0][1], P2Intentions[0][0]
+		_2PIntention[1][0], _2PIntention[1][1] = P2Intentions[1][1], P2Intentions[1][0]
 		
 		#各エージェントの現在位置取得+行動先取得
 		#エージェント関係のリストは基本的に[[1P_1],[1P_2]]のような形です
-		Agent1P_Current_Vertexes = [self._1PAgents[0]._point , self._1PAgents[1]._point]
-		Agent1P_Moving_Vertexes = [Agent1P_Current_Vertexes[0] + self._1PIntention[0] , Agent1P_Current_Vertexes[1] + self._1PIntention[1]]
-		Agent2P_Current_Vertexes = [self._2PAgents[0]._point , self._2PAgents[1]._point]
-		Agent2P_Moving_Vertexes = [Agent2P_Current_Vertexes[0] + self._2PIntention[0] , Agent2P_Current_Vertexes[1] + self._2PIntention[1]]
+		Agent1P_Current_Positions = [self._1PAgents[0]._point , self._1PAgents[1]._point]
+		Agent1P_Next_Positions = [Agent1P_Current_Positions[0] + _1PIntention[0] , Agent1P_Current_Positions[1] + _1PIntention[1]]
+		Agent2P_Current_Positions = [self._2PAgents[0]._point , self._2PAgents[1]._point]
+		Agent2P_Next_Positions = [Agent2P_Current_Positions[0] + _2PIntention[0] , Agent2P_Current_Positions[1] + _2PIntention[1]]
 
 
 		#メモ　味方同士で移動・パネル除去先がダブる→両者何もできないのでそのプレイヤーサイドの処理はすっ飛ばす
 		#　　　敵と味方で移動先がダブる→各プレイヤーかたっぽのエージェントは動ける可能性がある
 		#　　　タイル除去しようとしたが、停留されている→これもおそらくMoving_Vertexがかぶるから上の処理と同じかな
 		
-		Can_Agent1P_Move = [True,True]
-		Can_Agent2P_Move = [True,True]
+		Can_Agent1P_Move = [True, True]
+		Can_Agent2P_Move = [True, True]
 
-		if np.allclose(Agent1P_Moving_Vertexes[0] , Agent1P_Moving_Vertexes[1]):
-			Can_Agent1P_Move[False,False]
+		if np.allclose(Agent1P_Next_Positions[0] , Agent1P_Next_Positions[1]):
+			Can_Agent1P_Move[False, False]
 
-		if np.allclose(Agent2P_Moving_Vertexes[0] , Agent2P_Moving_Vertexes[1]):
-			Can_Agent2P_Move[False,False]
+		if np.allclose(Agent2P_Next_Positions[0] , Agent2P_Next_Positions[1]):
+			Can_Agent2P_Move[False, False]
 
 		for i in range(2):
 			for k in range(2):
-				Can_Agent1P_Move[i] = not np.allclose(Agent1P_Moving_Vertexes[i] , Agent2P_Moving_Vertexes[k])
+				Can_Agent1P_Move[i] = not np.allclose(Agent1P_Next_Positions[i] , Agent2P_Next_Positions[k])
 				Can_Agent2P_Move[k] = Can_Agent1P_Move[i]
 
 		#1Pサイドの処理
 		for i in range(2):
 			if not Can_Agent1P_Move[i]:
 				continue
-			Operated_panel = self._Panels[Agent1P_Moving_Vertexes[i][0] , Agent2P_Moving_Vertexes[i][1]]
+			Operated_panel = self._Panels[Agent1P_Next_Positions[i][0] , Agent2P_Next_Positions[i][1]]
 
 			if Operated_panel.getState() == 0 or Operated_panel.getState() == 1:
 				_1PAgents[i].move(_1PIntention[i])
@@ -162,7 +165,7 @@ class Game:
 		for i in range(2):
 			if not Can_Agent2P_Move[i]:
 				continue
-			Operated_panel = self._Panels[Agent2P_Moving_Vertexes[i][0] , Agent2P_Moving_Vertexes[i][1]]
+			Operated_panel = self._Panels[Agent2P_Next_Positions[i][0] , Agent2P_Next_Positions[i][1]]
 
 			if Operated_panel.getState() == 0 or Operated_panel.getState() == 2:
 				_2PAgents[i].move(_2PIntention[i])
