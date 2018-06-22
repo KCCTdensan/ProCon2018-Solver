33f45e146c3ef967a89e6d52bea6b485d92732f3
kamejiro <r116309@g.kobe-kosen.ac.jp>
2018-06-22 23:30:09 +0900

    Game.score()α版

--- Game.py
+++ Game.py
@@ -55,6 +55,63 @@ class Game:
 		self._Panels[_yLen - 1 - Agenty][Agentx] = Panel(0)
 		self._Panels[Agenty][_xLen - 1 - Agentx] = Panel(0)
 		self._Panels[_yLen - 1 - Agenty][_xLen - 1 - Agentx] = Panel(0)
+
+	def UpdatePanelSurrounded(self):
+		NumY = len(self._Panels)
+		NumX = len(self._Panels[0])
+		checkedPanel = np.zeros(NumY, NumX)
+
+		def set(x:int, y:int, surrounded:int):
+			if (checkedPanel[y][x] == 2) or (self._Panels[y][x].getState() != 0):
+				return
+			self._Panels[y][x].setSurrounded(surrounded)
+			checkedPanel[y][x] = 2
+			if x > 0:
+				set(x - 1, y)
+			if y > 0:
+				set(x, y - 1)
+			if x < NumX - 1:
+				set(x + 1, y)
+			if y < NumY - 1:
+				set(x, y + 1)
+
+		def check(x, y)->int:
+			if checkedPanel[y][x] != 0:
+				return -1
+
+			checkedPanel[y][x] = 1
+			panelState = self._Panels[y][x].getState()
+			if panelState != 0:
+				return panelState
+
+			if (x == 0) or (x == NumX - 1) or (y == 0) or (y == NumY - 1):
+				set(x, y, 0)
+				return 0
+
+			t = check(x, y + 1)
+			if t == 0:
+				return 0
+			r = check(x + 1, y)
+			if r == 0:
+				return 0
+			b = check(x, y - 1)
+			if b == 0:
+				return 0
+			l = check(x - 1, y)
+			if l == 0:
+				return 0
+			if (t == r) and (t == b) and (t == l):
+				return t
+			else:
+				set(x, y, 0)
+				return 0
+
+		for y in range(NumY):
+			for x in range(NumX):
+				if checkedPanel[y][x] == 0:
+					ret = check(x, y)
+					if ret != 0:
+						set(x, y, ret)
 			
 	def score(self): #得点計算
 		regionPoint1 = 0
@@ -65,59 +122,26 @@ class Game:
 		self._1Pscore = 0
 		self._2Pscore = 0
 
-		def regionPoint(self, x:int, y:int, Team)->int:
-			if searchedPanels[y][x]:
-				return 0
-			searchedPanels[y][x] = True
-			if (y == 0)or(y == NumY - 1)or(x == 0)or(x == NumX - 1):
-				return -1
-			if self._Panels[y][x].getState() == Team:
-				return -2
-			l = regionPoint(self, x - 1, y, Team)
-			if l == -1:
-				return -1
-			elif l == -2:
-				return abs(self._Panels[y][x].getScore())
-			t = regionPoint(self, x, y - 1, Team)
-			if t == -1:
-				return -1
-			elif l == -2:
-				return abs(self._Panels[y][x].getScore())
-			r = regionPoint(self, x + 1, y, Team)
-			if r == -1:
-				return -1
-			elif l == -2:
-				return abs(self._Panels[y][x].getScore())
-			b = regionPoint(self, x, y + 1, Team)
-			if b == -1:
-				return -1
-			elif l == -2:
-				return abs(self._Panels[y][x].getScore())
-			return l + t + r + b
+		UpdatePanelSurrounded()
 
 		for y in range(NumY):
 			for x in range(NumX):
-				p = self._Panels[y][x]
-				state = p.getState()
-				if state == 1:
-					self._1Pscore += p.getScore()
-				elif state == 2:
-					self._2Pscore += p.getScore()
-		#for y in range(NumY):
-		#	for x in range(NumX):
-		#		if not searchedPanels[y][x]:
-		#			regionPoint1 = regionPoint(self, x, y, 1)
-		#			if regionPoint1 >= 0:
-		#				self._1Pscore += regionPoint1
-		#for y in range(NumY):
-		#	for x in range(NumX):
-		#		if not searchedPanels[y][x]:
-		#			regionPoint2 = regionPoint(self, x, y, 2)
-		#			if regionPoint2 >= 0:
-		#				self._2Pscore += regionPoint2
+				panel = self._Panels[y][x]
+				panelState = panel.getState()
+				panelScore = panel.getScore()
+				panelSurrounded = panel.getSurrounded()
+				if panelState == 0:
+					if panelSurrounded == 1:
+						self._1Pscore += abs(panelScore)
+					elif panelSurrounded == 2:
+						self._2Pscore += abs(panelScore)
+				elif panelState == 1:
+					self._1Pscore += panelScore
+				elif panelState == 2:
+					self._2Pscore += panelScore
 
-	def action(self,P1Intentions:list, P2Intentions:list): #エージェントの意思をみて，実際に移動orパネル操作
-		#引数 P1Intensions:[[x,y],[x,y]]、P2Intentions:[[x,y],[x,y]]
+	def action(self, P1Intentions:list, P2Intentions:list): #エージェントの意思をみて，実際に移動orパネル操作
+		#引数 P1Intensions:[[x, y], [x, y]]、P2Intentions:[[x, y], [x, y]]
 
 		Intentions = [[P1Intentions[0][1], P1Intentions[0][0]], [P1Intentions[1][1], P1Intentions[1][0]], [P2Intentions[0][1], P2Intentions[0][0]], [P2Intentions[1][1], P2Intentions[1][0]]]
 		CurrentPositions = [self._1PAgents[0]._point, self._1PAgents[1]._point, self._2PAgents[0]._point, self._2PAgents[1]._point]
@@ -136,14 +160,14 @@ class Game:
 		for i in range(3):
 			for j in range(i + 1, 4):
 				tmp = not np.allclose(NextPositions[i], NextPositions[j])
-				CanMove[i] = CanMove[i]and tmp
-				CanMove[j] = CanMove[j]and tmp
+				CanMove[i] = CanMove[i] and tmp
+				CanMove[j] = CanMove[j] and tmp
 		for i in range(4):
 			if not CanMove[i]:
 				continue
 			py = NextPositions[i][0]
 			px = NextPositions[i][1]
-			CanMove[i] = (0 <= py)and(py < NumY)and(0 <= px)and(px < NumX)
+			CanMove[i] = (0 <= py) and (py < NumY) and (0 <= px) and (px < NumX)
 
 		#移動またはパネルを返す
 		for i in range(4):
@@ -151,7 +175,7 @@ class Game:
 				continue
 			OperatedPanel = self._Panels[NextPositions[i][0]][NextPositions[i][1]]
 			State = OperatedPanel.getState()
-			if State == 0 or State == Team[i]:
+			if (State == 0) or (State == Team[i]):
 				OperatedPanel.mkcard(Team[i])
 				Agents[i].move(Intentions[i])
 			else:
--- Panel.py
+++ Panel.py
@@ -1,7 +1,8 @@
 class Panel:
 	def __init__(self, score:int): #パネル生成
 		self._score = score #パネルが持つ点数
-		self._state = 0 #パネルの状態(0が中立，1が1P，2が2P)
+		self._state = 0 #パネルの状態(0:中立，1:1P，2:2P)
+		self._surrounded = 0 #パネルが囲まれているか(0:囲まれていない, 1:1Pのパネルに囲まれている, 2:2Pのパネルに囲まれている)
 	
 	def mkcard(self, team:int): #パネルにカードを置く(team:チーム)
 		self._state = team
@@ -14,3 +15,9 @@ class Panel:
 
 	def getState(self):
 		return self._state
+
+	def getSurrounded(self)->int:
+		return self._surrounded
+
+	def setSurrounded(self, team):
+		self._surrounded = team
