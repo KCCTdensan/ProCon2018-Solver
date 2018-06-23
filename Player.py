4c03ba2c2b86a557f5cb1b40a75345ce47399499
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-06-24 00:01:27 +0900

    AIBase->Player

--- Player.py
+++ Player.py
@@ -1,7 +1,7 @@
-class AIBase():
+class Player():#人，AIの継承用クラス
 	self._intention : [int, int] #2つのエージェントの動かし方
 
-	def bestMove(self):#盤面の情報を渡して最適なAgentの動かし方を返す
+	def intention(self):#盤面の情報を渡して最適なAgentの動かし方を返す
 		return self._Intention
 
 	def getResult(self):#結果を渡す(学習に使う?)
