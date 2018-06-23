03378dab8450374450425c48ba3a5205221fa92d
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-06-23 23:37:28 +0900

    AIの継承用クラスAIBase

---
+++ AIBase.py
@@ -0,0 +1,8 @@
+class AIBase():
+	self._intention : [int, int] #2つのエージェントの動かし方
+
+	def bestMove(self):#盤面の情報を渡して最適なAgentの動かし方を返す
+		return self._Intention
+
+	def getResult(self):#結果を渡す(学習に使う?)
+		return
