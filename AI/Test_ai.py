f8a24a4ca0f16e444301c97a15400c82c5113aca
RemonJuice <31856897+RemonJuice@users.noreply.github.com>
2018-09-13 15:50:07 +0900

    test_ai.py追加

---
+++ AI/Test_ai.py
@@ -0,0 +1,6 @@
+import random
+
+class AI(Player):
+    def intention(self,Game:Game):#盤面の情報を渡してAgentの動かし方を返す
+        self._Intention = [[random.randint(0,1),random.randint(0,1),random.randint(0,1)][random.randint(0,1),random.randint(0,1),random.randint(0,1)]]
+        return self._Intention
