19f7317506a15497c2691b791ee52bda15923d8c
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-10-14 15:49:24 +0900

    iroiro

--- Engine/kerasDQN_ai.py
+++ Engine/kerasDQN_ai.py
@@ -1,3 +1,4 @@
+import random
 from Player import Player
 from kerasDQN_model import(
     buildModel, train, Evaluate  
@@ -6,19 +7,30 @@ from kerasDQN_model import(
 class kerasDQNPlayer(Player):
     def __init__(self):
         self._model = buildModel()
-        self._GameLog = [] #行動の記録保存先
+        self._GAMMA = 0.01
+        self._EPSILON = 0.3
+        self._GameImgLog = [] #盤面の記録保存先
+        self._GameIntentionLog = [] #行動の記録保存先
 
     def intention(self, Game):#盤面の情報を渡してAgentの動かし方を返す
         self._intentions = searchIntentions(Game) #可能な行動を全て探す
         self._GameImg = getGameImg(Game) #盤面を画像データに
+        self._Intention = [[0, 0, 0],[0 ,0 ,0]]
 
-        self._maxEvalue = 0
-        for intention in self._intentions: #評価値が一番高い行動を選択
-            self.Q = Evaluate(self._model, self._GameImg, intention) #行動の評価値計算
-            if self._maxEvalue < self.Q:
-                self._maxEvalue = self.Q
-                self._Intention = intention
-        self._GameLog += [self._GameImg, self._Intention] #盤面と行動を記録
+        if(random.random() < self._EPSILON):
+            #ランダムに行動を選択
+            self._Intention = self._intentions[random.randint(0,len(self._intentions))]
+        else:
+            #評価値が一番高い行動を選択
+            self._maxEvalue = -1
+            for intention in self._intentions: 
+                self.Evalue = Evaluate(self._model, self._GameImg, intention) #行動の評価値計算
+                if(self._maxEvalue < self.Evalue):
+                    self._maxEvalue = self.Evalue
+                    self._Intention = intention
+
+        self._GameImgLog += self._GameImg #盤面と行動を記録
+        self._GameIntentionLog += self._Intention
         return self._Intention
 
     def searchIntention(Game): #可能な行動を全て探す
@@ -29,5 +41,29 @@ class kerasDQNPlayer(Player):
         GameImg = []
         return GameImg
 
-    def getResult(self):#行動の記録を残す
+    def learn(self, reword):#対戦データを学習
+        Qs = [reword]
+        for i in range(len(self._GameImgLog)-1):
+            Q.insert(0, self._GAMMA*Q[i])
+
+        model = buildModel()
+        model.compile(
+            loss="mean_squared_error",
+            optimizer="adam"
+            )
+        if(os.path.isfile("./checkpoint/model_params")):
+            model.load_weights("./checkpoint/model_params")
+        model.summary()
+
+        train(
+            model,
+            self._GameImgLog,
+            self._GameIntentionLog,
+            self._GameImgLog[0],
+            self._GameIntentionLog[0],
+            Qs,
+            10000,
+            )
+
+    def outputLog(self, reword):#行動の記録を残す
         pass
--- Engine/kerasDQN_model.py
+++ Engine/kerasDQN_model.py
@@ -1,17 +1,17 @@
 ﻿import os
-import datatime
+import datetime
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten
-from keras.callbacks import TensorBoard, Modelcheckpoint, EarlyStopping
+from keras.layers import Input, Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten, Multiply
+from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
 from keras.losses import mean_squared_error
 
 def buildModel():
-    board_input = Input(shape = [14, 14, 3])
+    board_input = Input(shape = [14, 14, 2])
     action_input = Input(shape = [3,2])
-    x = Conv2D(14, 3)(board_input)
+    x = Conv2D(14, 2)(board_input)
     x = Activation("relu")(x)
-    x = Conv2D(14, 3)(board_input)
+    x = Conv2D(14, 2)(board_input)
     x = Activation("relu")(x)
     x = MaxPooling2D(pool_size=(2,2))(x)
 
@@ -21,6 +21,8 @@ def buildModel():
 
     x = Flatten()(x)
     x = Dense(1024, activation="relu")(x)
+    y = Dense(1024, activation="relu")(action_input)
+    x = Multiply()([x, y])
     x = Dropout(2.0)(x)
     
     output = Dense(6, activation="softmax")(x)
@@ -28,16 +30,16 @@ def buildModel():
     model = Model(inputs=[board_input,action_input],outputs=output)
     return model
 
-def train(model, x_train, y_train, val_dataflow, max_epochs, patience):
+def train(model, x_train, y_train, val_x, val_y, max_epochs):
     timestamp = datetime.datetime.now()
 
-    cp_dir = "./checkpoint/{:%Y%m%d_%H%M%S}".format(timestamp)
+    cp_dir = "./checkpoint".format(timestamp)
     if not os.path.exists(cp_dir):
         os.makedirs(cp_dir)
-    cp_filepath = os.path.join(cp_dir, "model_{epoch:06d}.h5")
+    cp_filepath = os.path.join(cp_dir, "model_params.h5")
     cb_mc = ModelCheckpoint(filepath=cp_filepath, monitor="val_loss", period=1, save_best_only=True)
 
-    cb_es = EarlyStopping(monitor="val_loss", patience=patience)
+    #cb_es = EarlyStopping(monitor="val_loss", patience=patience)
 
     tb_log_dir = "./tensorboard/{:%Y%m%d_%H%M%S}".format(timestamp)
     cb_tb = TensorBoard(log_dir=tb_log_dir)
@@ -45,10 +47,10 @@ def train(model, x_train, y_train, val_dataflow, max_epochs, patience):
     model.fit(
         x=x_train,
         y=y_train,
-        epochs=max_epochs,
+        #epochs=max_epochs,
         verbose=2,
         validation_data=(val_x, val_y),
-        callbacks=[cb_mc, cb_es, cb_tb])
+        callbacks=[cb_mc, cb_tb])
 
 def Evaluate(self, model, img, Intention): #行動の評価値を算出
     return model.predict([img, Intention], verbose=1)
