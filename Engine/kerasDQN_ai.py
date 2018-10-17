a2d7082947336439bfde3415756250cfddf01f5e
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-10-17 23:12:14 +0900

    fix iroiro

--- Engine/kerasDQN_ai.py
+++ Engine/kerasDQN_ai.py
@@ -10,18 +10,18 @@ class kerasDQNPlayer(Player):
         self._team = team
         self._model = buildModel()
         self._GAMMA = 0.01
-        self._EPSILON = 0.3
+        self._EPSILON = 1
         self._GameImgLog = [] #盤面の記録保存先
         self._GameIntentionLog = [] #行動の記録保存先
 
     def intention(self, Game):#盤面の情報を渡してAgentの動かし方を返す
-        intentions = searchIntentions(Game) #可能な行動を全て探す
-        GameImg = getGameImg(Game) #盤面を画像データに
+        intentions = self.searchIntentions(Game) #可能な行動を全て探す
+        GameImg = self.getGameImg(Game) #盤面を画像データに
         goodIntention = np.zeros((2, 3), int) 
 
         if(random.random() < self._EPSILON):
             #ランダムに行動を選択
-            goodIntention = intentions[random.randint(0,len(intentions))]
+            goodIntention = intentions[random.randint(0,len(intentions)-1)]
         else:
             #評価値が一番高い行動を選択
             maxEvalue = -1
@@ -31,11 +31,11 @@ class kerasDQNPlayer(Player):
                     maxEvalue = Evalue
                     goodIntention = intention
 
-        self._GameImgLog += GameImg #盤面と行動を記録
-        self._GameIntentionLog += goodIntention
+        self._GameImgLog.append(GameImg) #盤面と行動を記録
+        self._GameIntentionLog.append(goodIntention)
         return goodIntention
 
-    def searchIntention(self, Game): #可能な行動を全て探す
+    def searchIntentions(self, Game): #可能な行動を全て探す
         intentions = []
         for i in range(-1, 1):
             for j in range(-1, 1):
@@ -53,7 +53,7 @@ class kerasDQNPlayer(Player):
             Game.canAction(pairintention[1], self._team)
         return intentions
 
-    def getGameImg(Game): #盤面を画像に
+    def getGameImg(self, Game): #盤面を画像に
         GameImg = np.zeros((12, 12, 2), int)
         Panels = Game.getPanels()
         for i in range(len(Panels)):
@@ -61,7 +61,7 @@ class kerasDQNPlayer(Player):
                 Panel = Panels[i][j]
                 GameImg[i][j][0] = Panel.getScore()
                 if(Panel.getState() != 0):
-                    GameImg[i][j][1] = Panel.getState()*Panel.getSurrounded()[Panel.getstate()-1]
+                    GameImg[i][j][1] = Panel.getState()*Panel.getSurrounded()[Panel.getState()-1]
         return GameImg
 
     def learn(self, reword):#対戦データを学習
