ff6c86ed2a5b193c6e67ee6092e98e1394bf15e1
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-10-22 22:59:27 +0900

    AutoFight

--- Main.py
+++ Main.py
@@ -1,11 +1,16 @@
 import wx
 from Window import WindowFrame
+from AutoFight import AutoFight
 
 def main():
 	app = wx.App(False)
 	frame = WindowFrame()
 	frame.Show()
 	app.MainLoop()
+
+def main2():
+    A = AutoFight()
+    A.AutoFight(1000)
 
 if __name__ == '__main__':
 	main()
