fc997c3c691d83d85d9fe39f35d2f24e4d9782bb
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-10-24 00:57:08 +0900

    Input(10,12,12)->output(81)に修正

--- Engine/kerasDQN_ai.py
+++ Engine/kerasDQN_ai.py
@@ -12,11 +12,6 @@ class kerasDQNPlayer(Player):
     def __init__(self, team):
         self._team = team
         self._model = buildModel()
-        self._GAMMA = 0.01
-        self._EPSILON = 0.3 
-        self._GameImgLog = [] #盤面の記録保存先
-        self._GameIntentionLog = [] #行動の記録保存先
-        self._maxQs = []
 
         #GPU使用率決定
         self._config = tf.ConfigProto()
@@ -24,102 +19,43 @@ class kerasDQNPlayer(Player):
         set_session(tf.Session(config=self._config))
 
     def intention(self, Game):#盤面の情報を渡してAgentの動かし方を返す
-        intentions = self.searchIntentions(Game) #可能な行動を全て探す
         GameImg = self.getGameImg(Game) #盤面を画像データに
-        goodIntention = np.zeros((2, 3), int) 
 
         #評価値が一番高い行動を選択
         maxEvalue = -1
-        for intention in intentions: 
-            Evalue = Evaluate(self._model, GameImg, intention) #行動の評価値計算
-            if(maxEvalue < Evalue):
-                maxEvalue = Evalue
-                goodIntention = intention
+        Agent1Intention = 0
+        Agent2Intention = 0
+        print(Evaluate(self._model, GameImg)) 
+        Evalues = Evaluate(self._model, GameImg).reshape(9, 9) #行動の評価値計算
+        for i in Evalues:
+            for j in Evalues[0]:
+                if Evalues[i][j] > maxEvalue:
+                    Agent1Intention = i
+                    Agent2Intention = j
 
-        if(random.random() < self._EPSILON):
-            #ランダムに行動を選択
-            goodIntention = intentions[random.randint(0,len(intentions)-1)]
-
-        self._GameImgLog.append(GameImg) #盤面と行動を記録
-        self._GameIntentionLog.append(goodIntention)
-        self._maxQs.append(maxEvalue)
         return goodIntention
 
-    def searchIntentions(self, Game): #可能な行動を全て探す
-        intentions = []
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                intentions.append([i,j,0])
-                intentions.append([i,j,1])
-
-        pairintentions = []
-        for i in intentions:
-            for j in intentions:
-                pairintentions.append([i, j])
-        return pairintentions
-
-        for pairintention in pairintentions:
-            Game.canAction(pairintention[0], self._team-1)
-            Game.canAction(pairintention[1], self._team)
-        return intentions
-
     def getGameImg(self, Game): #盤面を画像に
-        GameImg = np.zeros((12, 12, 2), int)
+        GameImg = np.random.rand(10, 12, 12)
+        print(GameImg)
         Panels = Game.getPanels()
-        for i in range(len(Panels)):
-            for j in range(len(Panels[0])):
-                Panel = Panels[i][j]
-                GameImg[i][j][0] = Panel.getScore()
-                if(Panel.getState()== 0):pass
-                elif(Panel.getState() == self._team):
-                    GameImg[i][j][1] = 1
-                elif(Panel.getState() != self._team):
-                    GameImg[i][j][1] = 3
-                if((Panel.getSurrounded()==[True, False]) | (Panel.getSurrounded()==[False, True])):
-                    GameImg[i][j][1] += 1
         return GameImg
 
-    def getAllReward(self, reward):#総報酬の計算
-        #報酬rのリストを作成
-        turn = len(self._GameImgLog)
-        rs = np.zeros(turn)
-        rs[len(rs)-1] = reward
-
-        Rs = np.hstack(rs.reshape([-1,1])).astype(float)
-        for i in range(turn-1):
-            Rs[i] = self._GAMMA * (rs[i] + self._maxQs[i+1] - self._maxQs[i])
-        Rs[turn-1] = reward
-        return Rs.reshape([-1,1])
-
-    def learn(self, reward, train_x1=None, train_x2=None, train_y=None):#対戦データを学習
-        xImgs = np.array(self._GameImgLog).reshape([-1,12,12,2])
-        xIntentions = np.array(self._GameIntentionLog).reshape([-1,2,3])
-        yRs = self.getAllReward(reward)
-
-        if((train_x1!=None)&(train_x2!=None)&(train_y!=None)):
-            xImgs = np.array(train_x1).reshape([-1,12,12,2])
-            xIntentions = np.array(train_x2).reshape([-1,2,3])
-            yRs = np.array(train_y).reshape([-1,1])
-
+    def learn(self, train_x, train_y, val_x, val_y):#対戦データを学習
         self._model.compile(
             loss="mean_squared_error",
             optimizer="adam"
             )
-        if(os.path.isfile("./checkpoint/model_params")):
-            model.load_weights("./checkpoint/model_params")
+        #if(os.path.isfile("./checkpoint/model_params")):
+        #    model.load_weights("./checkpoint/model_params")
 
         train(
             self._model,
             #train_data 
-            xImgs, 
-            xIntentions,
-            yRs,
+            train_x, 
+            train_y,
             #val_data
-            xImgs, 
-            xIntentions,
-            yRs,
-            100,
+            val_x, 
+            val_y,
+            1000,
             )
-
-    def outputLog(self, reword):#行動の記録を残す
-        pass
--- Engine/kerasDQN_model.py
+++ Engine/kerasDQN_model.py
@@ -2,82 +2,73 @@
 import datetime
 import numpy as np
 from keras.models import Model
-from keras.layers import Input, Dense, Dropout, Conv2D, MaxPooling2D, Activation, Flatten, Concatenate, BatchNormalization 
+from keras.layers import Input, Dense, Dropout, Conv2D, Activation, Flatten, Concatenate, BatchNormalization 
 from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
 from keras.losses import mean_squared_error
 
 def buildModel():
-    board_input = Input((12, 12, 2))
-    action_input = Input((2,3))
+    input = Input((10, 12, 12))
 
-    x = Conv2D(12, 2, input_shape=(12, 12, 2))(board_input)
+    x = Conv2D(12, 2, padding="same", data_format="channels_first", input_shape=(10, 12, 12))(input)
     x = Activation("relu")(x)
-    x = Conv2D(12, 2)(board_input)
+    x = Conv2D(12, 2, padding="same")(x)
     x = Activation("relu")(x)
-    x = MaxPooling2D(pool_size=(2,2))(x)
-
-    x = Conv2D(24, 3)(board_input)
+    x = Conv2D(24, 3, padding="same")(x)
     x = Activation("relu")(x)
-    x = MaxPooling2D(pool_size=(2,2))(x)
-
     x = Flatten()(x)
+
     x = Dense(1024, activation="relu")(x)
-    y = Flatten()(action_input)
-    y = Dense(1024, activation="relu")(y)
-    x = Concatenate(-1)([x, y])
+    x = BatchNormalization()(x)
     x = Dense(512, activation="relu")(x)
     x = BatchNormalization()(x)
-    x = Dense(128, activation="relu")(x)
+    x = Dense(256, activation="relu")(x)
     x = BatchNormalization()(x)
     x = Dropout(2.0)(x)
+
+    output = Dense(81, activation="relu")(x)
     
-    output = Dense(1, activation="tanh")(x)
-    
-    model = Model(inputs=[board_input,action_input],outputs=output)
+    model = Model(inputs=input, outputs=output)
     return model
 
-def train(model, x_train1, x_train2, y_train, val_x1, val_x2, val_y, epochs):
+def train(model, x_train, y_train, val_x, val_y, epochs):
     timestamp = datetime.datetime.now()
 
-    cp_dir = "./checkpoint".format(timestamp)
+    cp_dir = "./checkpoint/{:%Y%m%d_%H%M%S}".format(timestamp)
     if not os.path.exists(cp_dir):
         os.makedirs(cp_dir)
-    cp_filepath = os.path.join(cp_dir, "model_params.h5")
-    #cb_mc = ModelCheckpoint(filepath=cp_filepath, monitor="val_loss", period=1, save_best_only=True)
-    cb_mc = ModelCheckpoint(filepath=cp_filepath, period=1, save_best_only=True)
+    cp_filepath = os.path.join(cp_dir, "model_{epoch:06d}.h5")
+    cb_mc = ModelCheckpoint(filepath=cp_filepath, monitor="val_loss", period=1, save_best_only=True)
 
-    #cb_es = EarlyStopping(monitor="val_loss", patience=patience)
+    #cb_es = EarlyStopping(monitor="val_loss")
 
     tb_log_dir = "./tensorboard/{:%Y%m%d_%H%M%S}".format(timestamp)
     cb_tb = TensorBoard(log_dir=tb_log_dir)
 
-    x_train1 = x_train1.reshape(-1, 12, 12, 2).astype("float32")/16.0
-    x_train2 = x_train2.reshape(-1, 2, 3).astype("float32")
-    y_train = y_train.reshape(-1, 1).astype("float32")
+    x_train = x_train.reshape(-1, 10, 12, 12).astype("float32")/16.0
+    y_train = y_train.reshape(-1, 81).astype("float32")
 
-    val_x1 = val_x1.reshape(-1, 12, 12, 2).astype("float32")/16.0
-    val_x2 = val_x2.reshape(-1, 2, 3).astype("float32")
-    val_y = val_y.reshape(-1, 1).astype("float32")
+    val_x = val_x.reshape(-1, 10, 12, 12).astype("float32")/16.0
+    val_y = val_y.reshape(-1, 81).astype("float32")
     
-    Dataflow=trainDataGenerator(x_train1, x_train2, y_train)
+    train_Dataflow=trainDataGenerator(x_train, y_train)
+    val_Dataflow=trainDataGenerator(val_x, val_y)
 
     model.fit_generator(
-        Dataflow,
-        steps_per_epoch=16,
-        epochs=100,
+        train_Dataflow,
+        steps_per_epoch=10000,
+        epochs=epochs,
         callbacks=[cb_mc, cb_tb],
-        validation_data=None,
-        validation_steps=None,
+        validation_data=val_Dataflow,
+        validation_steps=100,
         shuffle=True
         )
 
-def Evaluate(model, img, intention): #行動の評価値を算出
-    img = np.array(img).reshape(-1,12,12,2).astype("float32")/16.0
-    intention = np.array(intention).reshape(-1,2,3).astype("float32")
-    return model.predict([img, intention], verbose=0)
+def Evaluate(model, img): #行動の評価値を算出
+    img = np.array(img).reshape(-1,10,12,12).astype("float32")/16.0
+    return model.predict(img, verbose=0)
 
-def trainDataGenerator(x_train1, x_train2, y_train):
+def trainDataGenerator(x_train, y_train):
     while True:
-        nData = len(x_train1)
+        nData = len(x_train)
         i = np.random.randint(nData)
-        yield [x_train1[i:i+1,:], x_train2[i:i+1,:]],y_train[i:i+1,:]
+        yield x_train[i:i+1,:], y_train[i:i+1,:]
