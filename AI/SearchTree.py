e23753ff9726da9438b1fe5518a9494c88af8008
RemonJuice <31856897+RemonJuice@users.noreply.github.com>
2018-10-25 22:20:00 +0900

    Search.py内容追加(仮)

--- AI/SearchTree.py
+++ AI/SearchTree.py
@@ -2,93 +2,48 @@
 from Panel import *
 from Agent import *
 from Game import *
+from Node import *
 
 class tree_search_ai:
-	__init__(self,stage &Stage)
-	CurrentNode = new friend_node(Stage);
+	__init__(self,Stage)
+	CurrentNode = friend_node(Stage)
 
-tree_search_ai::~tree_search_ai()
-{
-	delete CurrentNode;
-	CurrentNode = nullptr;
-}
+def NumTurns(NumTurns:char):
+	ChangeNumTurns(NumTurns);
 
-void tree_search_ai::NumTurns(unsigned char NumTurns)
-{
-	node::ChangeNumTurns(NumTurns);
-}
+def BestMove(IntentionIDs[NumTeams][stage.NumAgents]:action_id):
+	for intentions in IntentionIDs:
+		for i in intentinos:
+			i = -1
 
-int tree_search_ai::BestMove(action_id(&IntentionIDs)[NumTeams][stage::NumAgents])
-{
-	int Result[ID_MaxID][ID_MaxID];
-	int Ret;
-
-	for(team_no t = 0; t < NumTeams; ++t)
-	{
-		for(char a = 0; a < stage::NumAgents; ++a)
-		{
-			IntentionIDs[t][a] = -1;
-		}
-	}
-
-	CurrentNode->Search(node::NumCallPlay);
-	CurrentNode->Result(Result);
-	int Max = 0;
-	for (action_id i = 0; i < ID_MaxID; ++i)
-	{
-		for (action_id j = 0; j < ID_MaxID; ++j)
-		{
-			if (Max < Result[i][j])
-			{
-				Max = Result[i][j];
-				IntentionIDs[Team_1P][0] = i;
-				IntentionIDs[Team_1P][1] = j;
-			}
-		}
-	}
-	/*if(IntentionIDs[Team_1P][0] == -1)
-	{
-		IntentionIDs[Team_1P][0] = ID_Stay;
-		IntentionIDs[Team_1P][1] = ID_Stay;
-	}*/
-
-	Ret = Result[IntentionIDs[Team_1P][0]][IntentionIDs[Team_1P][1]];
-
-	CurrentNode->ChildNode(IntentionIDs[Team_1P])->Search(node::NumCallPlay);
-	CurrentNode->ChildNode(IntentionIDs[Team_1P])->Result(Result);
+	CurrentNode.Search(NumCallPlay);
+	CurrentNode.Result(Result);
 	Max = 0;
-	for (action_id i = 0; i < ID_MaxID; ++i)
-	{
-		for (action_id j = 0; j < ID_MaxID; ++j)
-		{
-			if (Max < Result[i][j])
-			{
-				Max = Result[i][j];
-				IntentionIDs[Team_2P][0] = i;
-				IntentionIDs[Team_2P][1] = j;
-			}
-		}
-	}
-	/*if(IntentionIDs[Team_2P][0] == -1)
-	{
-		IntentionIDs[Team_2P][0] = ID_Stay;
-		IntentionIDs[Team_2P][1] = ID_Stay;
-	}*/
+	for i in range(ID_MaxID):
+		for i in range(ID_MaxID):
+			if Max < Result[i][j]:
+				Max = Result[i][j]
+				IntentionIDs[Team_1P][0] = i
+				IntentionIDs[Team_1P][1] = j
 
+	Ret = Result[IntentionIDs[Team_1P][0]][IntentionIDs[Team_1P][1]]
+
+	CurrentNode.ChildNode(IntentionIDs[Team_1P]).Search(NumCallPlay)
+	CurrentNode.ChildNode(IntentionIDs[Team_1P]).Result(Result)
+	Max = 0;
+	for i in range(ID_MaxID):
+		for j in range(ID_MaxID):
+			if Max < Result[i][j]:
+				Max = Result[i][j]
+				IntentionIDs[Team_2P][0] = i
+				IntentionIDs[Team_2P][1] = j
 	return Ret;
-}
 
-void tree_search_ai::Move(action_id(&IntentionIDs)[NumTeams][stage::NumAgents])
-{
-	CurrentNode = CurrentNode->UpdateCurrentNode(IntentionIDs);
-}
+def Move(IntentionIDs[NumTeams][stage::NumAgents]:action_id):
+	CurrentNode = CurrentNode.UpdateCurrentNode(IntentionIDs)
 
-const stage& tree_search_ai::GetStage()const
-{
-	return CurrentNode->GetStage();
-}
+def GetStage():
+	return CurrentNode.GetStage()
 
-void tree_search_ai::PrintStage()const
-{
-	CurrentNode->PrintStage();
-}
+def PrintStage():
+	CurrentNode.PrintStage()
