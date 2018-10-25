781f3f74fb3232c9bc17c5fee0b51ada7c70d625
RemonJuice <31856897+RemonJuice@users.noreply.github.com>
2018-10-25 22:03:43 +0900

    ファイル追加、クラス、関数定義

---
+++ AI/Node.py
@@ -0,0 +1,78 @@
+﻿from Game import *
+
+class node:
+	def __init__(self):
+		pass
+	def ChangeNumTurns(self,NumTurns:int):
+		pass
+	def ChangeCntTurns(CntTurns):
+		pass
+	def IsLeafNode():
+		return false
+
+class friend_node:
+	def __init__(Stage):
+		pass
+	def __init__(Stage,CntTurns:char):
+		pass
+	def Search(NumCallPlay:int):
+		pass
+	def Result(Result[ID_MaxID][ID_MaxID]):
+		pass
+	def ChildNode(IntentionID1,IntentionID2):
+		pass
+	def ChildNode(action_id[stage::NumAgents]):
+		pass
+	def UpdateCurrentNode(action_id(&SelectIntentionIDs)[NumTeams][stage::NumAgents]):
+		pass
+	def NextNode(action_id[NumTeams][stage::NumAgents]):
+		pass
+	def GetStage();
+		pass
+	def PrintStage():
+		pass
+	def PrintChildNodeInfo():
+		pass
+
+
+	def Play():
+		pass
+	def Selection():
+		pass
+	def Expansion():
+		pass
+	def Evaluation():
+		pass
+	def Rollout(NumTurn:int):
+		pass
+	def ClearChildNode():
+		pass
+	def UCB1(Q:float,NChild:int):
+		pass
+
+class opponent_node:
+
+	def Play():
+		pass
+	def Selection():
+		pass
+	def Expansion():
+		pass
+	def ClearChildNode():
+		pass
+	def UCB1(float Q, int NChild):
+		pass
+
+	def opponent_node(*ParentNode,Intentions[stage::NumAgents]:action_id,CntTurns:char)
+		pass
+	def Search(NumCallPlay:int):
+		pass
+	def Result(Result[ID_MaxID][ID_MaxID]:int[][]):
+		pass
+	def ChildNode(action_id IntentionID1, action_id IntentionID2):
+		pass
+	def ChildNode(action_id(&IntentionIDs)[stage::NumAgents]):
+		pass
+
+	def PrintChildNodeInfo():
+		pass
---
+++ AI/SearchTree.py
@@ -0,0 +1,94 @@
+﻿import numpy
+from Panel import *
+from Agent import *
+from Game import *
+
+class tree_search_ai:
+	__init__(self,stage &Stage)
+	CurrentNode = new friend_node(Stage);
+
+tree_search_ai::~tree_search_ai()
+{
+	delete CurrentNode;
+	CurrentNode = nullptr;
+}
+
+void tree_search_ai::NumTurns(unsigned char NumTurns)
+{
+	node::ChangeNumTurns(NumTurns);
+}
+
+int tree_search_ai::BestMove(action_id(&IntentionIDs)[NumTeams][stage::NumAgents])
+{
+	int Result[ID_MaxID][ID_MaxID];
+	int Ret;
+
+	for(team_no t = 0; t < NumTeams; ++t)
+	{
+		for(char a = 0; a < stage::NumAgents; ++a)
+		{
+			IntentionIDs[t][a] = -1;
+		}
+	}
+
+	CurrentNode->Search(node::NumCallPlay);
+	CurrentNode->Result(Result);
+	int Max = 0;
+	for (action_id i = 0; i < ID_MaxID; ++i)
+	{
+		for (action_id j = 0; j < ID_MaxID; ++j)
+		{
+			if (Max < Result[i][j])
+			{
+				Max = Result[i][j];
+				IntentionIDs[Team_1P][0] = i;
+				IntentionIDs[Team_1P][1] = j;
+			}
+		}
+	}
+	/*if(IntentionIDs[Team_1P][0] == -1)
+	{
+		IntentionIDs[Team_1P][0] = ID_Stay;
+		IntentionIDs[Team_1P][1] = ID_Stay;
+	}*/
+
+	Ret = Result[IntentionIDs[Team_1P][0]][IntentionIDs[Team_1P][1]];
+
+	CurrentNode->ChildNode(IntentionIDs[Team_1P])->Search(node::NumCallPlay);
+	CurrentNode->ChildNode(IntentionIDs[Team_1P])->Result(Result);
+	Max = 0;
+	for (action_id i = 0; i < ID_MaxID; ++i)
+	{
+		for (action_id j = 0; j < ID_MaxID; ++j)
+		{
+			if (Max < Result[i][j])
+			{
+				Max = Result[i][j];
+				IntentionIDs[Team_2P][0] = i;
+				IntentionIDs[Team_2P][1] = j;
+			}
+		}
+	}
+	/*if(IntentionIDs[Team_2P][0] == -1)
+	{
+		IntentionIDs[Team_2P][0] = ID_Stay;
+		IntentionIDs[Team_2P][1] = ID_Stay;
+	}*/
+
+	return Ret;
+}
+
+void tree_search_ai::Move(action_id(&IntentionIDs)[NumTeams][stage::NumAgents])
+{
+	CurrentNode = CurrentNode->UpdateCurrentNode(IntentionIDs);
+}
+
+const stage& tree_search_ai::GetStage()const
+{
+	return CurrentNode->GetStage();
+}
+
+void tree_search_ai::PrintStage()const
+{
+	CurrentNode->PrintStage();
+}
