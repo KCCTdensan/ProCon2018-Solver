ae66417aad4a4d7b1ca02bb0dc39d060c682bd3f
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-10-25 18:31:26 +0900

    学習開始

--- Engine/kerasDQN_model.py
+++ Engine/kerasDQN_model.py
@@ -10,9 +10,9 @@ from keras.losses import mean_squared_error
 def buildModel():
     input = Input((10, 12, 12))
 
-    x = Conv2D(12, 2, padding="same", data_format="channels_first", input_shape=(10, 12, 12))(input)
+    x = Conv2D(12, 3, padding="same", data_format="channels_first", input_shape=(10, 12, 12))(input)
     x = Activation("relu")(x)
-    x = Conv2D(12, 2, padding="same")(x)
+    x = Conv2D(12, 3, padding="same")(x)
     x = Activation("relu")(x)
     x = Conv2D(24, 3, padding="same")(x)
     x = Activation("relu")(x)
@@ -24,15 +24,16 @@ def buildModel():
     x = BatchNormalization()(x)
     x = Dense(256, activation="relu")(x)
     x = BatchNormalization()(x)
-    #x = Dropout(2.0)(x)
+    x = Dropout(0.5)(x)
 
-    output = Dense(81, activation="relu")(x)
+    output1 = Dense(9, activation="softmax")(x)
+    output2 = Dense(9, activation="softmax")(x)
     
-    model = Model(inputs=input, outputs=output)
+    model = Model(inputs=input, outputs=[output1,output2])
     #model.summary()
     model.compile(
             loss="categorical_crossentropy",
-            optimizer=Adam(lr=0.0001),
+            optimizer="adam",
             metrics=["accuracy"]
             )
     return model
@@ -52,10 +53,10 @@ def train(model, x_train, y_train, val_x, val_y, epochs):
     cb_tb = TensorBoard(log_dir=tb_log_dir)
 
     x_train = x_train.reshape(-1, 10, 12, 12).astype("float32")/16.0
-    y_train = y_train.reshape(-1, 81).astype("float32")/2.0
+    y_train = y_train.reshape(-1, 9, 9).astype("float32")
 
     val_x = val_x.reshape(-1, 10, 12, 12).astype("float32")/16.0
-    val_y = val_y.reshape(-1, 81).astype("float32")/2.0
+    val_y = val_y.reshape(-1, 9, 9).astype("float32")
     
     train_Dataflow=trainDataGenerator(x_train, y_train, 100)
     val_Dataflow=trainDataGenerator(val_x, val_y, 100)
@@ -67,7 +68,6 @@ def train(model, x_train, y_train, val_x, val_y, epochs):
         callbacks=[cb_mc, cb_tb],
         validation_data=val_Dataflow,
         validation_steps=100,
-        shuffle=True
         )
 
 def Evaluate(model, img): #行動の評価値を算出
@@ -82,4 +82,12 @@ def trainDataCreater(x_train, y_train, batch_size):
     offsets = np.random.randint(len(x_train), size=batch_size)
     batch_x = np.stack(x_train[offset] for offset in offsets)
     batch_y = np.stack(y_train[offset] for offset in offsets)
-    return batch_x, batch_y
+    y_train1 = np.zeros((len(batch_y),9), "float32")
+    y_train2 = np.zeros((len(batch_y),9), "float32")
+    for k in range(len(batch_y)):
+        for i in range(len(batch_y[0])):
+            for j in range(len(batch_y[0][0])):
+                if(batch_y[k][i][j] == 1):
+                    y_train1[k][j] = 1.0
+                    y_train2[k][i] = 1.0
+    return batch_x, [y_train1, y_train2]
