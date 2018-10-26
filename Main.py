713e6121d0392e0b3f61858c0d627d6b3b4da1a6
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-10-27 00:52:24 +0900

    main fix

--- Main.py
+++ Main.py
@@ -21,8 +21,9 @@ def main3():
 	dir_path=("./bin/")
 	TrainingData=ReadTrainingDatasFromDirectory(dir_path)
 	train_x, val_x = np.split(TrainingData[0], [int(len(TrainingData[0])/10*9)])
-	train_y, val_y = np.split(TrainingData[1], [int(len(TrainingData[1])/10*9)])
-	AI.learn(train_x, train_y, val_x, val_y)
+	train_y1, val_y1 = np.split(TrainingData[1], [int(len(TrainingData[1])/10*9)])
+	train_y2, val_y2 = np.split(TrainingData[2], [int(len(TrainingData[2])/10*9)])
+	AI.learn(train_x, train_y1, train_y2, val_x, val_y1, val_y2)
 
 if __name__ == '__main__':
 	main3()
