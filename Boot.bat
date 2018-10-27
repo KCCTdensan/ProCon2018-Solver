4dc6461287ae88fb397b6ad6578f8f90eca9aabf
kcct-dk <kcctdensan@gmail.com>
2018-10-27 19:43:26 +0900

    多環境対応

--- Boot.bat
+++ Boot.bat
@@ -1,4 +1,5 @@
-move "C:\Users\%username%\Pictures\Camera Roll\*.jpg" %~dp0QRCode.jpg
+:move "C:\Users\%username%\Pictures\Camera Roll\*.jpg" %~dp0QRCode.jpg
+move "C:\Users\%username%\Pictures\ƒJƒƒ‰ ƒ[ƒ‹\*.jpg" %~dp0QRCode.jpg
 
 python Main.py
 
