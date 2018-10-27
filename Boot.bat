dfcf3fe7b8670cc9e082da1dbb52af85eb64857d
kcct-dk <kcctdensan@gmail.com>
2018-10-27 19:54:10 +0900

    更新

--- Boot.bat
+++ Boot.bat
@@ -1,6 +1,15 @@
-:move "C:\Users\%username%\Pictures\Camera Roll\*.jpg" %~dp0QRCode.jpg
-move "C:\Users\%username%\Pictures\カメラ ロール\*.jpg" %~dp0QRCode.jpg
+@echo off
+SET EN=C:\Users\%username%\Pictures\Camera Roll\
+SET JA=C:\Users\%username%\Pictures\カメラ ロール\
+@echo on
+
+IF EXIST "%EN%" (
+	move "%EN%*.jpg" %~dp0QRCode.jpg
+) ELSE (
+	move "%JA%*.jpg" %~dp0QRCode.jpg
+)
 
 python Main.py
 
+@echo off
 cmd /k
